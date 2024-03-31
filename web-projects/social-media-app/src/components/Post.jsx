de1d39573d7d135bb15f8448d9b7478add0fab49
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 15:40:15 -0400

    added functional button

--- web-projects/social-media-app/src/components/Post.jsx
+++ web-projects/social-media-app/src/components/Post.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import Comment from './Comment';
 
 function Post() {
+  let likes = 30;
+
+  function handleLike() {
+    console.log('Liked!');
+  }
+
   return (
     <div>
       <h3>Test Post</h3>
       <p>This is a test post we'll edit later!</p>
-      <p>Likes: 30</p>
-      <button>Like</button>
+      <p>Likes: {likes}</p>
+      <button onClick={handleLike}>Like</button>
       <p>Comments:</p>
       <Comment content="This is a test comment!" />
       <Comment content="This is another test comment!" />
