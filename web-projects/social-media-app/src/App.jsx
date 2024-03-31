66f5312a72a6df4551e95cdf27e4b25ec91868bf
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 15:25:07 -0400

    added code app

--- web-projects/social-media-app/src/App.jsx
+++ web-projects/social-media-app/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Post from './components/Post'; // This imports the Post component from the specified path
 
-function App() {
-  return (
-    <div>
-      <h1>Fakebook!</h1>
-      {/* CreatePostForm */}
-      {/* Feed */}
-      {/* Post */}
-    </div>
-  );
+function App() { // Corrected function declaration
+    return (
+        <div>
+            <h1>Fakebook!</h1>
+      
+            {/* These comments indicate where you might add more components in the future */}
+            {/* CreatePostForm */}
+            {/* Feed */}
+
+            <Post /> {/* This is where the Post component is used in the JSX */}
+        </div>
+    );
 }
 
-export default App;
+export default App; // This exports the App component as the default export of this module
