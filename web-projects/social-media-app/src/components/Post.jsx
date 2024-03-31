69c0218de70310d8f524bd56b6fa955e29cc0f1d
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 15:43:37 -0400

    fully functional button

--- web-projects/social-media-app/src/components/Post.jsx
+++ web-projects/social-media-app/src/components/Post.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Comment from './Comment';
 
 function Post() {
-  let likes = 30;
+  // Initialize likes using useState
+  const [likes, setLikes] = useState(30);
 
+  // Update the likes state when the button is clicked
   function handleLike() {
-    console.log('Liked!');
+    setLikes(likes + 1);
   }
 
   return (
     <div>
       <h3>Test Post</h3>
       <p>This is a test post we'll edit later!</p>
-      <p>Likes: {likes}</p>
+      <p>Likes: {likes}</p> {/* Display the likes from state */}
       <button onClick={handleLike}>Like</button>
       <p>Comments:</p>
       <Comment content="This is a test comment!" />
