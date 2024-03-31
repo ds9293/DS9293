86b91a5fdf74a4b6f5681651537d54fcd0e6e434
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 16:04:04 -0400

    added code to Feed

---
+++ web-projects/social-media-app/src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from './Post';
+
+function Feed({ posts }) {
+  return (
+    <div>
+      {posts.map((post, index) => (
+        <Post key={index} content={post.content} />
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
