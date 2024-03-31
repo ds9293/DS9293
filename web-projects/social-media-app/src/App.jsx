1ae2861c4d47c0f479beea1f428114a6bb79c33b
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 16:05:11 -0400

    added feed and createpostform to app

--- web-projects/social-media-app/src/App.jsx
+++ web-projects/social-media-app/src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import Post from './components/Post';
+import React, { useState } from 'react';
+import CreatePostForm from './CreatePostForm';
+import Feed from './Feed';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  const addPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+
   return (
     <div>
       <h1>Fakebook!</h1>
-      {/* CreatePostForm */}
-      {/* Feed */}
-      <Post content="This is a test post!" />
-      <Post content="This is another test post!" />
+      <CreatePostForm addPost={addPost} />
+      <Feed posts={posts} />
     </div>
   );
 }
