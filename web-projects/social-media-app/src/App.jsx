3feef02c7f1a8e7d499de2895d3c9c969c71f2d6
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-03-31 14:55:09 -0400

    new web-projects and social-media-app

---
+++ web-projects/social-media-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Fakebook!</h1>
+      {/* CreatePostForm */}
+      {/* Feed */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default App;
