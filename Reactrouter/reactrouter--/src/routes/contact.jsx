e0adfd0e415f90c0e9db12989af61df0cb2f7599
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-04-25 20:53:38 -0400

    update 8

--- Reactrouter/reactrouter--/src/routes/contact.jsx
+++ Reactrouter/reactrouter--/src/routes/contact.jsx
@@ -1,20 +1,23 @@
 import { Form, useLoaderData } from "react-router-dom";
 import { getContact } from "../contacts";
 
-
+export async function loader({ params }) {
+    const contact = await getContact(params.contactId);
+    return { contact };
+}
 
 export default function Contact() {
     const { contact: loadedContact } = useLoaderData();
 
-    // Provide default values if contact properties are undefined
-    const contact = {
-        first: loadedContact?.first || "Your",
-        last: loadedContact?.last || "Name",
-        avatar: loadedContact?.avatar || "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/438099663_7514262395286023_7396021035072627314_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lUn71B8GZU8Q7kNvgEIz2vn&_nc_ht=scontent-lga3-2.xx&oh=00_AfAKx34uo42O8nfvu1lwG85xi9Tb06RmaJx3qxhFdeKCJg&oe=66309D42",
-        twitter: loadedContact?.twitter || "your_handle",
-        notes: loadedContact?.notes || "Some notes",
-        favorite: typeof loadedContact?.favorite === 'boolean' ? loadedContact.favorite : true,
-    };
+        const contact = {
+          first: "Your",
+          last: "Name",
+          avatar: "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/438099663_7514262395286023_7396021035072627314_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lUn71B8GZU8Q7kNvgEIz2vn&_nc_ht=scontent-lga3-2.xx&oh=00_AfAKx34uo42O8nfvu1lwG85xi9Tb06RmaJx3qxhFdeKCJg&oe=66309D42",
+          twitter: "your_handle",
+          notes: "Some notes",
+          favorite: true,
+        };
+    
 
 
   return (
@@ -94,4 +97,4 @@ function Favorite({ contact }) {
       </button>
     </Form>
   );
-}
+    }
