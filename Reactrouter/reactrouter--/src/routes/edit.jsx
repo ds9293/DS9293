a69760f53385f952f617a2c9052c6176a02c7097
ds9293 <96140790+ds9293@users.noreply.github.com>
2024-04-28 16:39:17 -0400

    update 13

--- Reactrouter/reactrouter--/src/routes/edit.jsx
+++ Reactrouter/reactrouter--/src/routes/edit.jsx
@@ -3,7 +3,11 @@ import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
     const formData = await request.formData();
+    const firstName = formData.get("first");
+  const lastName = formData.get("last");
     const updates = Object.fromEntries(formData);
+    updates.first; // "Some"
+updates.last; // "Name"
     await updateContact(params.contactId, updates);
     return redirect(`/contacts/${params.contactId}`);
   }
